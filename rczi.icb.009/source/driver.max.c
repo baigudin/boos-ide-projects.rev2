e97fccaf6730cd93b9d378bd4c5b7b93c81c0424
Sergey Baigudin <sergey@baigudin.software>
2017-06-14 22:12:25 +0300

    ISDOING

--- rczi.icb.009/source/driver.max.c
+++ rczi.icb.009/source/driver.max.c
@@ -25,6 +25,21 @@
 #define REG_MAX_PAGESEL (0x1f)
 
 /**
+ * Mask of register address.
+ */
+#define REG_ADDR_MASK (0x001f)
+
+/**
+ * Mask of register page.
+ */
+#define REG_PAGE_MASK (0x0003)
+
+/**
+ * Mask of register address.
+ */
+#define REG_PAGE_SHIFT (5)
+
+/**
  * Reset pin of MCU port.
  */
 sbit reset_ = REG_P1^2;
@@ -47,14 +62,23 @@ static int8 isInitialized_;
  */
 int16 maxRead(enum RegMax regAddr)
 {
-  uint16 addr = regAddr & 0x1f;
-  uint16 page = regAddr >> 5 & 0x3;
-  if(page != page_)
-  {
-    mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, page | 0x10);
-    page_ = page;
+  int16 value;
+  uint16 addr = regAddr & REG_ADDR_MASK;
+  uint16 page = regAddr >> REG_PAGE_SHIFT & REG_PAGE_MASK;
+  if(isInitialized_)
+  {   
+    if(page != page_)
+    {
+      mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, page | 0x0010);
+      page_ = page;
+    }
+    value = mdioRead(PHY_ADDR, addr);  
   }
-  return mdioRead(PHY_ADDR, addr);  
+  else
+  {
+    value = -1;
+  }
+  return value;
 }
 
 /**
@@ -65,14 +89,17 @@ int16 maxRead(enum RegMax regAddr)
  */
 void maxWrite(enum RegMax regAddr, int16 value)
 {
-  uint16 addr = regAddr & 0x1f;
-  uint16 page = regAddr >> 5 & 0x3;
-  if(page != page_)
-  {
-    mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, page | 0x10);
-    page_ = page;
+  uint16 addr = regAddr & REG_ADDR_MASK;
+  uint16 page = regAddr >> REG_PAGE_SHIFT & REG_PAGE_MASK;
+  if(isInitialized_)
+  {   
+    if(page != page_)
+    {
+      mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, page | 0x0010);
+      page_ = page;
+    }
+    mdioWrite(PHY_ADDR, addr, value);
   }
-  mdioWrite(PHY_ADDR, addr, value);
 }
 
 /**
@@ -106,16 +133,42 @@ int8 maxInit(void)
     }      
   }
   /* Set zero register page */
-  mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, 0x10);
+  mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, 0x0010);
   page_ = 0;  
   /* The device ID checking */
   if(error == BOOS_OK)
   {
-    val = maxRead(REG_MAX_ID);
-    if(val & 0x0fff != 0x0ee0)
+    mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, 0x0011);    
+    val = mdioRead(PHY_ADDR, REG_MAX_ID & REG_ADDR_MASK);
+    /* Revision A */
+    if((val & 0x0fff) == 0x0edf)
     {
-      error = BOOS_ERROR;
+      /* Set zero register page */
+      mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, 0x0010);
+      page_ = 0;        
+    }
+    /* Revision B */    
+    else if((val & 0x0fff) == 0x0ee0)
+    {
+      /* Make the startup procedure */
+      /* Selects MDIO page 2 */
+      mdioWrite(PHY_ADDR, REG_MAX_PAGESEL , 0x0012);      
+      /* Write 0x4004 to PTPCR1 to power-down the receive CDR */
+      mdioWrite(PHY_ADDR, REG_MAX_PTPCR1 & REG_ADDR_MASK, 0x4004);            
+      /* Wait 1ms */
+      threadSleep(1);
+      /* Write 0x4000 to PTPCR1 to power-up the receive CDR */
+      mdioWrite(PHY_ADDR, REG_MAX_PTPCR1 & REG_ADDR_MASK, 0x4000);
+      /* Set the BMCR.DP_RST bit to reset the datapath */
+      mdioWrite(PHY_ADDR, REG_MAX_PAGESEL, 0x0010);
+      mdioWrite(PHY_ADDR, REG_MAX_BMCR & REG_ADDR_MASK, 0x8000);
+      val = mdioRead(PHY_ADDR, REG_MAX_BMCR & REG_ADDR_MASK);  
+      page_ = 0;
     }  
+    else
+    {
+      error = BOOS_ERROR;      
+    }
   }
   if(error == BOOS_OK)
   {
