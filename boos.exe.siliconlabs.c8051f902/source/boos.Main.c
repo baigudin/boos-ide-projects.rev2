044d07d0041061ad23759c54afc4a94a25ca0b56
Sergey Baigudin <baigudin@mail.ru>
2018-08-03 19:43:02 +0300

    New project create

---
+++ boos.exe.siliconlabs.c8051f902/source/boos.Main.c
@@ -0,0 +1,19 @@
+/** 
+ * User program entry.
+ * 
+ * @author    Sergey Baigudin, sergey@baigudin.software
+ * @copyright 2017 Sergey Baigudin
+ * @license   http://baigudin.software/license/
+ * @link      http://baigudin.software
+ */
+#include "boos.Main.h"
+
+/**
+ * User program entry.
+ *
+ * @return error code or zero.
+ */
+int8 userMain()
+{
+  return 0;
+}
